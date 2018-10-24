98c2b9114590dfc10c0b95708aced6bb17991fba
Unknown <kuz16@itu.edu.tr>
2018-10-24 14:59:08 +0300

    Add 'users' table for database initializer

--- dbinit.py
+++ dbinit.py
@@ -5,8 +5,29 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """CREATE TABLE IF NOT EXISTS public.users
+(
+    id serial NOT NULL UNIQUE,
+    first_name character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    creation_date timestamp without time zone NOT NULL DEFAULT now(),
+    birth_date timestamp without time zone NOT NULL,
+    post_karma integer NOT NULL DEFAULT 0,
+    comment_karma integer NOT NULL DEFAULT 0,
+    is_admin boolean NOT NULL DEFAULT false,
+    is_banned boolean NOT NULL DEFAULT false,
+    username character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_id_primary PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to itucs;""",
 ]
 
 
