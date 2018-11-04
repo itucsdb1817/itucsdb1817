8ef8fb80d15e55406442890c8f13dbe2f6fb9598
Unknown <kuz16@itu.edu.tr>
2018-11-04 15:42:04 +0300

    Add 'votes' and 'reports' tables

--- dbinit.py
+++ dbinit.py
@@ -28,6 +28,60 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.users
     OWNER to itucs;""",
+
+    """
+    CREATE TABLE IF NOT EXISTS public.votes
+(
+    id serial NOT NULL UNIQUE,
+    date timestamp without time zone NOT NULL,
+    parent_type character varying(32) COLLATE pg_catalog."default" NOT NULL,
+    passed_time interval NOT NULL,
+    vote boolean NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT vote_id_pk PRIMARY KEY (id),
+    CONSTRAINT user_id_fk FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.votes
+    OWNER to itucs;
+    """,
+    """
+    CREATE TABLE IF NOT EXISTS public.reports
+(
+    id serial NOT NULL UNIQUE,
+    violated_rule text COLLATE pg_catalog."default" NOT NULL,
+    date timestamp without time zone NOT NULL,
+    reason_description text COLLATE pg_catalog."default",
+    action_taken text COLLATE pg_catalog."default" NOT NULL,
+    is_dismissed boolean NOT NULL,
+    post_id integer,
+    comment_id integer,
+    CONSTRAINT report_id_pk PRIMARY KEY (id),
+    CONSTRAINT comment_id_fk FOREIGN KEY (comment_id)
+        REFERENCES public.comments (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT post_id_fk FOREIGN KEY (post_id)
+        REFERENCES public.posts (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.reports
+    OWNER to itucs;
+
+    """,
 ]
 
 
