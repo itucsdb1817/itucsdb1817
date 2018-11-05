8a2169d34f3ee095b732db912f2ca19fe6c7a234
Unknown <kuz16@itu.edu.tr>
2018-11-05 22:36:41 +0300

    Create register, login, logout routes for user

---
+++ routes/user/login.py
@@ -0,0 +1,8 @@
+from flask import request, jsonify, session
+import os
+import sys
+import psycopg2 as dbapi2
+
+def login():
+	return "login"
+
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 from flask import Flask
 
+from routes.user import login
+from routes.user import logout
+from routes.user import register
+from models import db
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    app.add_url_rule("/user/login", view_func=login.login)
+    app.add_url_rule("/user/logout",view_func=logout.logout)
+    app.add_url_rule("/user/register",view_func=register.register)
+    database = db.Database()
+    app.config["db"] = database
 
-@app.route("/")
-def home_page():
-    return "Hello, world!"
+    return app
 
 
 if __name__ == "__main__":
-    app.run()
+    app = create_app()
+    app.run(host="0.0.0.0", port=8080, debug=True)
