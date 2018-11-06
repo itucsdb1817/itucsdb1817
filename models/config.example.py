1c3f6ea636510b2b7de579059220f50f8d944332
ehakan <ehakan99@gmail.com>
2018-11-07 01:08:31 +0300

    Adapt database connection settings to work in a Heroku environment

--- models/config.example.py
+++
@@ -1,2 +0,0 @@
-def database():
-	return "postgres://db_username:db_password@localhost:db_port/db_name"
--- models/db.py
+++ models/db.py
@@ -1,12 +1,11 @@
 import os
 import sys
 import psycopg2 as dbapi2
-from . import config 
 class Database:
     def __init__(self):
         print("self")
     def execute(self, sql, args):
-        with dbapi2.connect(config.database()) as connection:
+        with dbapi2.connect(os.getenv("DATABASE_URL")) as connection:
             cursor = connection.cursor()
             cursor.execute(sql, args)
             self.cursor = cursor
