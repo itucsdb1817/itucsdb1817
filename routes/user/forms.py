04da9ec2ec619e4cbc6641fafb079a3210c24a2d
Unknown <kuz16@itu.edu.tr>
2018-11-11 14:39:18 +0300

    Add form classes for login and registration systems at forms.py
    Add html files for test

---
+++ routes/user/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField,DateTimeField
+from wtforms.validators import DataRequired
+from wtforms import validators
+from wtforms.fields.html5 import EmailField
+from datetime import datetime
+import os
+import sys
+
+#Form for user to log in.
+class LoginForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+
+
+#Registration form for user to sign up.
+#Restrictions may be implemented later.
+class RegistrationForm(FlaskForm):
+	firstname = StringField('First Name', validators=[DataRequired()])
+	lastname = StringField('Last Name', validators=[DataRequired()])
+	birth_date = DateTimeField('Birth Date',validators=[DataRequired()])
+	username = StringField('Username', validators=[DataRequired()])
+	email = EmailField('Email address', [validators.DataRequired(), validators.Email()])
+	password = PasswordField('New Password',  [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+	confirm = PasswordField('Repeat Password')
+	submit = SubmitField('Sign Up')
+
+
