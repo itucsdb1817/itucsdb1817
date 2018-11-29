b3e4b511b20e45d729e2ec08089f4aacb3746e9c
ehakan <ehakan99@gmail.com>
2018-11-29 16:14:08 +0300

    Define post_view workflow

--- routes/post/post_routes.py
+++ routes/post/post_routes.py
@@ -7,7 +7,13 @@ post_pages = Blueprint('post_pages', __name__, template_folder = 'templates')
 @post_pages.route('/post/<post_id>')
 def post_view(post_id):
     # get post info by id, 
-    # render post main
+    # render post block
+    # TO DO AFTER:
+    # expose comment input
+    # get all comments
+    # group comments (nesting)
+    # sort comments by opt
+
     NotImplementedError()
 
 @post_pages.route('/post/submit')
