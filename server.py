95a1370745a6ca0909f144f8426c45477779225f
ehakan <ehakan99@gmail.com>
2018-11-29 16:39:12 +0300

    Update config of secret keys

--- server.py
+++ server.py
@@ -10,12 +10,14 @@ def create_app():
 	app.config["bcrypt"] = Bcrypt(app)    
 	app.register_blueprint(user_routes.page)
 	app.config['DB_URL'] = os.getenv('DATABASE_URL')  
+
+	app.config['SECRET_KEY'] = 'AccioSecretKey'
+	app.config['WTF_CSRF_SECRET_KEY'] = 'AccioCSRFSecretKey'
 	 
 	return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.secret_key = 'secret_key'
     app.config['SESSION_TYPE']= 'filesystem'
     app.run(host="0.0.0.0", port=8080, debug=True)
