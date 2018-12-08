8e292140300eb47b8e3ebb8fcc201a4a34560adf
ehakan <ehakan99@gmail.com>
2018-12-08 20:09:22 +0300

    Redefine modules in post form

--- routes/post/post_forms.py
+++ routes/post/post_forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, fields, validators
+from wtforms import TextField, TextAreaField, validators
 
-class BasePostForm(Form):
-    title   = fields.TextField('Title', [validators.required(), validators.length(max=255, min=2)])
-    tag     = fields.TextField('Tag', [validators.required(), validators.length(max=32)])
+class BasePostForm(FlaskForm):
+    title   = TextField('Title', [validators.required(), validators.length(max=255, min=2)])
+    tag     = TextField('Tag', [validators.length(max=32)])
 
 class TextPostForm(BasePostForm):
-    content = fields.TextAreaField('Content', [validators.required()])
+    content = TextAreaField('Content', [validators.required()])
