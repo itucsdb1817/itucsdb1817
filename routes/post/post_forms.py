52fb496701d3c976744eb06602d4a4017bf8d832
ehakan <ehakan99@gmail.com>
2018-12-08 15:49:20 +0300

    Rename post form definitions

--- routes/post/post_forms.py
+++ routes/post/post_forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import Form, fields, validators
 
-class BasePost(Form):
+class BasePostForm(Form):
     title   = fields.TextField('Title', [validators.required(), validators.length(max=255, min=2)])
     tag     = fields.TextField('Tag', [validators.required(), validators.length(max=32)])
 
-class TextPost(BasePost):
+class TextPostForm(BasePostForm):
     content = fields.TextAreaField('Content', [validators.required()])
