08416c286a3150fb92c59d9846c14dba1e501720
Efe Hakan Gençoğlu <ehakan99@gmail.com>
2018-12-13 16:41:35 +0300

    Add methods to generate renderable comment trees

--- models/comment.py
+++ models/comment.py
@@ -1,6 +1,7 @@
 from flask import current_app
-from models.base import BaseModel
 import psycopg2 as db
+from models.base import BaseModel
+from models.user import User
 
 class Comment(BaseModel):
     TABLE_NAME = 'comments'
@@ -39,7 +40,7 @@ class Comment(BaseModel):
         """
         with db.connect(current_app.config['DB_URL']) as conn:
             cursor = conn.cursor()
-            query = "SELECT * FROM comments WHERE parent_id IS NOT NULL AND parent_id=%s"
+            query = f"SELECT * FROM {self.__class__.TABLE_NAME} WHERE parent_id IS NOT NULL AND parent_id=%s"
             cursor.execute(query, (self.id, ))
             results = cursor.fetchall()
             cursor.close()
@@ -51,3 +52,27 @@ class Comment(BaseModel):
 
         for child in self._children:
             child._get_children()
+
+    def _generate_context_comment(self):
+        return {
+            'id':       self.id,
+            'user':     User(self.user_id).username,
+            'user_id':  self.user_id,
+            'date':     self.date,
+            'vote':     self.current_vote,
+            'content':  self.content,
+            'children': []
+        }
+    
+    def generate_context(self):
+        """
+        Recursively generates the context needed for displaying the comments
+        """
+        context = self._generate_context_comment()
+        if hasattr(self, '_children'):
+            for child in self._children:
+                context['children'].append(child.generate_context())
+        return context
+
+
+
